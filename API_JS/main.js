b0493f08f8d53037bc68e6861e518908294110fd
YohannHoury <yohannhoury@ip-172-31-26-181.eu-west-3.compute.internal>
2022-07-06 11:35:34 +0000

    update link API

--- API_JS/main.js
+++ API_JS/main.js
@@ -1,40 +1,67 @@
 
-    document.addEventListener("DOMContentLoaded", function(event) { 
-    var allCustomersBtn = document.getElementById("load-all-customers-btn");
-    var tableBody = document.getElementById("all-customers-body");
+// easily create a <td> from a value
+function addTd($value)
+{
+    $td = document.createElement("td");
+    $val = document.createTextNode($value);
+    $td.appendChild($val);
     
+    return $td;
+}
+// when my DOM is ready
+document.addEventListener("DOMContentLoaded", function(event) { 
+    var $allCustomersBtn = document.getElementById("load-all-customers-btn");
+    var $tableBody = document.getElementById("all-customers-body")
     // when my button is clicked
-    allCustomersBtn.addEventListener("click", function(){
+    $allCustomersBtn.addEventListener("click", function(){
         
         // i'm calling my API with the correct GET parameters
-        fetch("https://yohannhoury.sites.3wa.io/DEVELOPPEMENT/PHP/PHP-API/YohannFakeFoodOrderAPI/index.php?route=all-customers", {
+         fetch("https://yohannhoury.sites.3wa.io/DEVELOPPEMENT/PHP/PHP-API/YohannFakeFoodOrderAPI/index.php?route=all-customers", {
             method: 'GET',
         })
             .then((response) => response.json())
             .then((responseJson) => {
                 console.log(responseJson);
-            })
-           });
                 
-     
+                $customers = responseJson; // this is not necessary but makes its easier to read
+                
                 // for each customer the API sent back
-               let customers = customers.forEach(function(item){
+                $customers.forEach(function($item){
                     
                     // i create a <tr>
-                    let row = document.createElement("tr");
-                    let id = addTd(item.id);
-                    let firstName = addTd(item.firstName);
-                    let lastName = addTd(item.lastName);
-                    let email = addTd(item.email);
+                    $row = document.createElement("tr");
+                    $id = addTd($item.id);
+                    $firstName = addTd($item.firstName);
+                    $lastName = addTd($item.lastName);
+                    $email = addTd($item.email);
                     
                     // i add the appropriate <td>s 
-                    row.appendChild(id);
-                    row.appendChild(firstName);
-                    row.appendChild(lastName);
-                    row.appendChild(email);
+                    $row.appendChild($id);
+                    $row.appendChild($firstName);
+                    $row.appendChild($lastName);
+                    $row.appendChild($email);
                     
                     // and I append it to my table
-                    $tableBody.appendChild(row);
+                    $tableBody.appendChild($row);
                 });
-            });
+            })
+            .catch((error) => { console.log(error); });
+    });
+
+    
+});
+function getVal() {
+  $login = document.querySelector('input').value;
+  console.log($login);
+}
+
+$form = new FormData(document.getElementById('login-form'));
+fetch("https://yohannhoury.sites.3wa.io/DEVELOPPEMENT/PHP/PHP-API/YohannFakeFoodOrderAPI/index.php?route=create-customer", {
+  method: "POST",
+  body: $form
+});
+
+
+
+
     
