4ff894a4fe39133971954f50007238afa04c0f9f
YohannHoury <yohannhoury@ip-172-31-26-181.eu-west-3.compute.internal>
2022-07-06 12:58:50 +0000

    update API

--- API_JS/main.js
+++ API_JS/main.js
@@ -1,67 +1,61 @@
 
-// easily create a <td> from a value
-function addTd($value)
-{
-    $td = document.createElement("td");
-    $val = document.createTextNode($value);
-    $td.appendChild($val);
-    
-    return $td;
-}
-// when my DOM is ready
-document.addEventListener("DOMContentLoaded", function(event) { 
-    var $allCustomersBtn = document.getElementById("load-all-customers-btn");
-    var $tableBody = document.getElementById("all-customers-body")
-    // when my button is clicked
-    $allCustomersBtn.addEventListener("click", function(){
         
         // i'm calling my API with the correct GET parameters
-         fetch("https://yohannhoury.sites.3wa.io/DEVELOPPEMENT/PHP/PHP-API/YohannFakeFoodOrderAPI/index.php?route=all-customers", {
+             fetch("https://api.quotable.io/random", {
             method: 'GET',
         })
             .then((response) => response.json())
             .then((responseJson) => {
                 console.log(responseJson);
-                
-                $customers = responseJson; // this is not necessary but makes its easier to read
-                
-                // for each customer the API sent back
-                $customers.forEach(function($item){
-                    
-                    // i create a <tr>
-                    $row = document.createElement("tr");
-                    $id = addTd($item.id);
-                    $firstName = addTd($item.firstName);
-                    $lastName = addTd($item.lastName);
-                    $email = addTd($item.email);
-                    
-                    // i add the appropriate <td>s 
-                    $row.appendChild($id);
-                    $row.appendChild($firstName);
-                    $row.appendChild($lastName);
-                    $row.appendChild($email);
-                    
-                    // and I append it to my table
-                    $tableBody.appendChild($row);
-                });
-            })
-            .catch((error) => { console.log(error); });
+               
+    });
+         
+         fetch("https://api.quotable.io/quotes?author=albert-einstein", {
+            method: 'GET',
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson);
+               
     });
 
-    
-});
-function getVal() {
-  $login = document.querySelector('input').value;
-  console.log($login);
-}
+          // i'm calling my API with the correct GET parameters
+         fetch("https://api.quotable.io/random?author=albert-einstein", {
+            method: 'GET',
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson);
+               
+    });
 
-$form = new FormData(document.getElementById('login-form'));
-fetch("https://yohannhoury.sites.3wa.io/DEVELOPPEMENT/PHP/PHP-API/YohannFakeFoodOrderAPI/index.php?route=create-customer", {
-  method: "POST",
-  body: $form
-});
+    fetch("https://api.quotable.io/random?author=abraham-lincoln", {
+            method: 'GET',
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson);
+               
+    });
 
 
+    fetch("https://api.quotable.io/random?author=abraham-lincoln", {
+            method: 'GET',
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson);
+               
+    });
 
+
+    fetch("https://api.quotable.io/search/quotes?query=wander+lost", {
+            method: 'GET',
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson);
+               
+    });
 
     
